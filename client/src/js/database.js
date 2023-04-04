a6a758fd013f2142319675283b96dad70306ed2e
ColtonTO <colton1105@gmail.com>
2023-04-04 12:38:00 -0700

    added entries into webpack. Added service worker js.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -25,7 +25,7 @@ export const putDb = async (content) => {
   const store = tx.objectStore('jate');
   
   // Use the .put() method to put data in the database.
-  const request = store.put({id: 1, value: content});
+  const request = store.put({ id: 1, value: content});
   
   // Get confirmation of the request.
   const result = await request;
@@ -54,7 +54,7 @@ export const getDb = async () => {
   // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result?.value;
 };
 
 initdb();
